bac94350fca51353b649141ad103302465735d56
Mateusz Byczkowski <byczkowski38@gmail.com>
2017-02-23 18:18:46 +0100

    next chapter commit

--- js/scrolling.js
+++ js/scrolling.js
@@ -11,10 +11,13 @@ $(document).ready(function () {
         }
         // find currently visible div :
         div = -1;
+        var count = 10;
         divs.each(function(i){
             if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
                 div = i;
-            }
+                count = i;
+                console.log(count);
+            }     
         });
         if (dir == 'up' && div > 0) {
             div--;
@@ -27,6 +30,12 @@ $(document).ready(function () {
             scrollTop: divs.eq(div).offset().top
         }, 1000, 'easeInOutExpo');
         $('#page-top').css({ opacity: 1});
+        if(count==1){    
+                $(".technology-item" ).addClass("animate-twist");               
+            }
+            else{
+                 $(".technology-item" ).removeClass("animate-twist");       
+            }
         return false;
     });
     $(window).resize(function () {
@@ -51,7 +60,18 @@ $(function() {
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        if($anchor.attr('href')=='#skills'){
+            $(".technology-item" ).addClass("animate-twist");      
+        }
+        else{
+             $(".technology-item" ).removeClass("animate-twist");   
+        }
+        if($anchor.attr('href')=='#about'){
+            $("#about-text" ).addClass("text-animate");      
+        }
+        else{
+             $("#about-text" ).removeClass("text-animate");   
+        }
     });
 });
-
         
