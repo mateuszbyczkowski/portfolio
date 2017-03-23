9de850a54fea003ff2e5ae8bab58a2b6c94628da
Mateusz Byczkowski <byczkowski38@gmail.com>
2017-03-23 10:41:07 +0100

    Portfolio

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,64 +1,3 @@
-//slide sub-pages on mouse-scroll feature
-$(document).ready(function () {
-    var divs = $('.slide-down');
-    var dir = 'up'; // wheel scroll direction
-    var div = 0; // current div
-    $(document.body).on('DOMMouseScroll mousewheel', function (e) {
-        if (e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0) {
-            dir = 'down';
-        } else {
-            dir = 'up';
-        }
-        // find currently visible div :
-        div = -1;
-        var count = 10;
-        divs.each(function(i){
-            if (div<0 && ($(this).offset().top >= $(window).scrollTop())) {
-                div = i;
-                count = i;
-                console.log(count);
-            }     
-        });
-        if (dir == 'up' && div > 0) {
-            div--;
-        }
-        if (dir == 'down' && div < divs.length) {
-            div++;
-        }
-        
-        $('html,body').stop().animate({
-            scrollTop: divs.eq(div).offset().top
-        }, 1000, 'easeInOutExpo');
-        $('#page-top').css({ opacity: 1});
-        if(count==1){    
-                $(".technology-item" ).addClass("animate-twist");               
-            }
-            else{
-                 $(".technology-item" ).removeClass("animate-twist");       
-            }
-        if(count==0){    
-             $("#about-text" ).addClass("text-animate");      
-            }
-            else{
-             $("#about-text" ).removeClass("text-animate");
-            }
-        if(count==2){    
-                $("#myCarousel" ).addClass("carousel-slide");      
-        }
-        else{
-             $("#myCarousel" ).removeClass("carousel-slide");   
-        }
-        if(count==3){
-            $("#left-img" ).addClass("figure-fix-left");   
-            $("#right-img" ).addClass("figure-fix-right");   
-        }
-        return false;
-    });
-    $(window).resize(function () {
-        $('html,body').scrollTop(divs.eq(div).offset().top);
-    });
-});
-
 //collapse the navbar on scroll
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -68,7 +7,7 @@ $(window).scroll(function() {
     }
 });
 
-//page scrolling feature
+//page scrolling feature+animation on click
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -76,32 +15,59 @@ $(function() {
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
-        if($anchor.attr('href')=='#skills'){
-            $(".technology-item" ).delay(200).addClass("animate-twist");      
-        }
-        else{
-             $(".technology-item" ).removeClass("animate-twist");   
-        }
-        if($anchor.attr('href')=='#about'){
-            $("#about-text" ).addClass("text-animate");      
-        }
-        else{
-             $("#about-text" ).removeClass("text-animate");   
-        }
-        if($anchor.attr('href')=='#projects'){
-            $("#myCarousel" ).addClass("carousel-slide");      
-        }
-        else{
-             $("#myCarousel" ).removeClass("carousel-slide");   
-        }
-        if($anchor.attr('href')=='#contact'){
-            $("#left-img" ).delay(200).addClass("figure-fix-left");   
-            $("#right-img" ).delay(200).addClass("figure-fix-right");   
-        }
-        else{
-            $("#left-img" ).removeClass("figure-fix-left");   
-            $("#right-img" ).removeClass("figure-fix-right");      
-        }     
     });
 });
         
+// Returns true if the specified element has been scrolled into the viewport.
+function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+}
+
+// Check if it's time to start the animation.
+function checkAnimation() {
+    var $technologyElement = $('.technology-item');
+    var $aboutElement = $('#about-text');
+    var $contactLeftElement = $('#left-img');
+    var $contactRightElement = $('#right-img');
+
+    if (isElementInViewport($technologyElement)) {
+        // Start the animation
+        $technologyElement.addClass('animate-twist');
+
+        $aboutElement.removeClass('text-animate');
+        $contactLeftElement.removeClass('figure-fix-left');
+        $contactRightElement.removeClass('figure-fix-right');
+    }
+    if (isElementInViewport($aboutElement)) {
+        // Start the animation
+        $aboutElement.addClass('text-animate');
+
+        $technologyElement.removeClass('animate-twist');
+        $contactLeftElement.removeClass('figure-fix-left');
+        $contactRightElement.removeClass('figure-fix-right');
+    }
+    if (isElementInViewport($contactLeftElement)) {
+        // Start the animation
+        $contactLeftElement.addClass('figure-fix-left');
+        $contactRightElement.addClass('figure-fix-right');
+
+        $technologyElement.removeClass('animate-twist');
+        $aboutElement.removeClass('text-animate');
+    }  
+}
+
+// Capture scroll events
+$(window).scroll(function(){
+    checkAnimation();
+});
